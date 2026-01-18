7923be716719ee4f827944199743f009935e4119
IcosagonZ <rohanraj3870z@gmail.com>
2026-01-18 11:20:10 +0530

    Added summary display for each data section

--- lib/pages/overview.dart
+++ lib/pages/overview.dart
@@ -59,34 +59,268 @@ class Page_Overview_State extends State<Page_Overview>
       ),
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
+        child: ListView(
+          children:[
             SvgPicture.asset(
               "assets/avatar.svg",
               fit: BoxFit.contain,
-              height: MediaQuery.of(context).size.height * 0.5,
+              height: MediaQuery.of(context).size.height * 0.45,
             ),
-            Expanded(
-              child: ListView(
-                children:[
-                  SizedBox(
-                    height: 32
+            SizedBox(
+              height: 32
+            ),
+            // Summary
+            Center(
+              child: Text("Summary", style:style_headlinemedium),
+            ),
+            SizedBox(
+              height: 8
+            ),
+            Center(
+              child: Text("All good", style:style_titlelarge),
+            ),
+            SizedBox(
+              height: 16
+            ),
+
+            // Time
+            // Time label
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text("Time", style:style_titlesmall),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Divider(),
+                )
+              ]
+            ),
+            SizedBox(
+              height: 8
+            ),
+
+            // Time details
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("8 hrs", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Slept", style:style_titlesmall),
+                    ]
                   ),
-                  Center(
-                    child: Text("Summary", style:style_headlinemedium),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("2 hrs", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Studied", style:style_titlesmall),
+                    ]
                   ),
-                  SizedBox(
-                    height: 8
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("3 hrs", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Free", style:style_titlesmall),
+                    ]
                   ),
-                  Center(
-                    child: Text("All good", style:style_titlelarge),
-                  )
-                ]
-              )
-            )
+                ),
+              ]
+            ),
+            SizedBox(
+              height: 16
+            ),
+
+            // Activity
+            // Activity label
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text("Activity", style:style_titlesmall),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Divider(),
+                )
+              ]
+            ),
+            SizedBox(
+              height: 8
+            ),
+
+            // Activity details
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("3124", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Steps", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("3.25 km", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Moved", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("54 mins", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Active", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+              ]
+            ),
+            SizedBox(
+              height: 16
+            ),
+
+            // Calorie
+            // Calorie label
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text("Calories", style:style_titlesmall),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Divider(),
+                )
+              ]
+            ),
+            SizedBox(
+              height: 8
+            ),
+
+            // Calorie details
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("2103 cal", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Consumed", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("2512 cal", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Burned", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("409 cal", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Deficit", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+              ]
+            ),
+            SizedBox(
+              height: 16
+            ),
+
+            // Body
+            // Body label
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Text("Body", style:style_titlesmall),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Divider(),
+                )
+              ]
+            ),
+            SizedBox(
+              height: 8
+            ),
+
+            // Body details
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("170 cm", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Height", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("20", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("BMI", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text("62 kg", style:style_headlinesmall),
+                      SizedBox(height: 8),
+                      Text("Weight", style:style_titlesmall),
+                    ]
+                  ),
+                ),
+              ]
+            ),
+            SizedBox(
+              height: 16
+            ),
           ]
-        ),
-      )
+        )
+      ),
     );
   }
 }
