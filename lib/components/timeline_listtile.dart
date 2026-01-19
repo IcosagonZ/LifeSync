1c6b62448c83a76c2b64f67ff7da87141bb727d0
IcosagonZ <rohanraj3870z@gmail.com>
2026-01-19 13:06:57 +0530

    Added basic things for timeline page

---
+++ lib/components/timeline_listtile.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class TimelineListTile extends StatelessWidget
+{
+  final Icon list_icon;
+  final String list_title;
+  final String list_subtitle;
+  final String list_date;
+
+  const TimelineListTile({
+    Key? key,
+    required this.list_icon,
+    required this.list_title,
+    required this.list_subtitle,
+    required this.list_date,
+  }) : super (key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    // Theming and text styles
+    final color_scheme = Theme.of(context).colorScheme;
+    final text_theme = Theme.of(context).textTheme;
+
+    Color color_primary = color_scheme.primary;
+    Color color_secondary = color_scheme.secondary;
+    Color color_onprimary = color_scheme.onPrimary;
+    Color color_onsecondary = color_scheme.onSecondary;
+    Color color_background = color_scheme.onBackground;
+    Color color_surface = color_scheme.onSurface;
+
+    final style_displaylarge = text_theme.displayLarge;
+    final style_displaymedium = text_theme.displayMedium;
+    final style_displaysmall = text_theme.displaySmall;
+
+    final style_headlinelarge = text_theme.headlineLarge;
+    final style_headlinemedium = text_theme.headlineMedium;
+    final style_headlinesmall = text_theme.headlineSmall;
+
+    final style_titlelarge = text_theme.titleLarge;
+    final style_titlemedium = text_theme.titleMedium;
+    final style_titlesmall = text_theme.titleSmall;
+
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 8,
+            ),
+            list_icon,
+            SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(list_title, textAlign: TextAlign.left, style: style_titlemedium),
+                  Text(list_subtitle)
+                ]
+              ),
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Text(list_date),
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+                PopupMenuItem(
+                  child: Text("View"),
+                  onTap: (){
+                    print("View tapped");
+                  },
+                ),
+                PopupMenuItem(
+                  child: Text("Edit"),
+                  onTap: (){
+                    print("Edit tapped");
+                  },
+                ),
+                PopupMenuItem(
+                  child: Text("Remove"),
+                  onTap: (){
+                    print("Remove tapped");
+                  },
+                ),
+              ],
+            )
+          ]
+        )
+      )
+      /*
+      child: ListTile(
+        leading: list_icon,
+        title: Text(list_title),
+        subtitle: Text(list_subtitle),
+        trailing:
+      ),
+      */
+    );
+  }
+}
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
 
+import '../components/timeline_listtile.dart';
+
 class Page_Timeline extends StatefulWidget
 {
   const Page_Timeline({super.key});
@@ -15,29 +17,6 @@ class Page_Timeline_State extends State<Page_Timeline>
   @override
   Widget build(BuildContext context)
   {
-    // Theming and text styles
-    final color_scheme = Theme.of(context).colorScheme;
-    final text_theme = Theme.of(context).textTheme;
-
-    Color color_primary = color_scheme.primary;
-    Color color_secondary = color_scheme.secondary;
-    Color color_onprimary = color_scheme.onPrimary;
-    Color color_onsecondary = color_scheme.onSecondary;
-    Color color_background = color_scheme.onBackground;
-    Color color_surface = color_scheme.onSurface;
-
-    final style_displaylarge = text_theme.displayLarge;
-    final style_displaymedium = text_theme.displayMedium;
-    final style_displaysmall = text_theme.displaySmall;
-
-    final style_headlinelarge = text_theme.headlineLarge;
-    final style_headlinemedium = text_theme.headlineMedium;
-    final style_headlinesmall = text_theme.headlineSmall;
-
-    final style_titlelarge = text_theme.titleLarge;
-    final style_titlemedium = text_theme.titleMedium;
-    final style_titlesmall = text_theme.titleSmall;
-
     // Widget size variables
 
     return Scaffold(
@@ -45,10 +24,15 @@ class Page_Timeline_State extends State<Page_Timeline>
         title: Text("Timeline"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: EdgeInsets.all(8),
         child: ListView(
           children:[
-            Text("Timeline")
+            TimelineListTile(
+              list_icon: Icon(Symbols.ecg_heart),
+              list_title: "Heartrate",
+              list_subtitle: "93 bpm",
+              list_date: "Unknown",
+            ),
           ]
         )
       ),
