609b54672e920d997232a02dfb25a692d52f9935
IcosagonZ <rohanraj3870z@gmail.com>
2026-01-21 23:02:37 +0530

    Added vital data entry widgets

--- lib/pages/add_data.dart
+++ lib/pages/add_data.dart
@@ -101,6 +101,25 @@ class Page_AddData_State extends State<Page_AddData>
   ];
   String? time_type_dropdown_chosen;
 
+  // Vital widgets
+  final TextEditingController vital_bodytemperature_controller = TextEditingController();
+  final TextEditingController vital_bloodoxygen_controller = TextEditingController();
+  final TextEditingController vital_bloodsugar_controller = TextEditingController();
+
+  final TextEditingController vital_bloodpressure_systolic_controller = TextEditingController();
+  final TextEditingController vital_bloodpressure_diastolic_controller = TextEditingController();
+
+  final TextEditingController vital_heartrate_controller = TextEditingController();
+
+  final List<String> vital_name_options = [
+    "Body temperature",
+    "Blood oxygen",
+    "Blood pressure",
+    "Blood sugar",
+    "Heartrate"
+  ];
+  String? vital_name_dropdown_chosen;
+
   @override
   void initState()
   {
@@ -938,12 +957,232 @@ class Page_AddData_State extends State<Page_AddData>
               )
             ),
 
+            // Vital data entry
+            Visibility(
+              visible: datatype_dropdown_chosen=="Vitals",
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text("Type")
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 100,
+                        ),
+                        child: DropdownButton<String>(
+                          hint: Text("Select..."),
+                          value: vital_name_dropdown_chosen,
+                          onChanged: (String? newValue)
+                          {
+                            setState(()
+                            {
+                              vital_name_dropdown_chosen = newValue;
+                              print(vital_name_dropdown_chosen);
+                            });
+                          },
+                          items: vital_name_options.map<DropdownMenuItem<String>>((String dropdown_item)
+                          {
+                            return DropdownMenuItem<String>(
+                              value: dropdown_item,
+                              child: Text(dropdown_item)
+                            );
+                          }
+                          ).toList(),
+                        ),
+                      ),
+                    ]
+                  ),
+                  SizedBox(height: 16),
+                  Visibility(
+                    visible: vital_name_dropdown_chosen=="Blood oxygen",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Blood oxygen percentage")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 30,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: vital_bloodoxygen_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("%"),
+                          ]
+                        ),
+                      ]
+                    )
+                  ),
+                  Visibility(
+                    visible: vital_name_dropdown_chosen=="Body temperature",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Body temperature")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 30,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: vital_bodytemperature_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("°C"),
+                          ]
+                        ),
+                      ]
+                    )
+                  ),
+                  Visibility(
+                    visible: vital_name_dropdown_chosen=="Blood pressure",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Blood pressure")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 30,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: vital_bloodpressure_systolic_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("/"),
+                            SizedBox(width: 16),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 30,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: vital_bloodpressure_diastolic_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ]
+                        ),
+                      ]
+                    )
+                  ),
+                  Visibility(
+                    visible: vital_name_dropdown_chosen=="Blood sugar",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Blood sugar")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 30,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: vital_bloodsugar_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("mmol/L"),
+                          ]
+                        ),
+                      ]
+                    )
+                  ),
+                  Visibility(
+                    visible: vital_name_dropdown_chosen=="Heartrate",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Heartrate")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 30,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: vital_heartrate_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("bpm"),
+                          ]
+                        ),
+                      ]
+                    )
+                  ),
+                  SizedBox(height: 16),
+                  Divider(),
+                ],
+              ),
+            ),
+
             // Notes
             SizedBox(height: 16),
             IntrinsicHeight(
               child: TextField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+                  border: UnderlineInputBorder(),
                   labelText: "Note",
                   hintText: "Enter note",
                 ),
