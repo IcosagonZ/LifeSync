bd70f033d677acae5e920892bcaf761940a62553
IcosagonZ <rohanraj3870z@gmail.com>
2026-01-21 22:38:13 +0530

    Added time entry widgets

--- lib/pages/add_data.dart
+++ lib/pages/add_data.dart
@@ -86,6 +86,21 @@ class Page_AddData_State extends State<Page_AddData>
   ];
   String? nutrition_type_dropdown_chosen;
 
+  // Time widgets
+  final TextEditingController time_duration_hours_controller = TextEditingController();
+  final TextEditingController time_duration_minutes_controller = TextEditingController();
+
+  final List<String> time_type_options = [
+    "Sleep",
+    "Study",
+    "Hobby",
+    "Gaming",
+    "Outing",
+    "Commute",
+    "Entertainment",
+  ];
+  String? time_type_dropdown_chosen;
+
   @override
   void initState()
   {
@@ -829,6 +844,92 @@ class Page_AddData_State extends State<Page_AddData>
                       ),
                       SizedBox(width: 16),
                       Text("g"),
+                    ]
+                  ),
+                  SizedBox(height: 16),
+                  Divider(),
+                ]
+              )
+            ),
+
+            // Time data entry
+            Visibility(
+              visible: datatype_dropdown_chosen=="Time",
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text("Type")
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 100,
+                        ),
+                        child: DropdownButton<String>(
+                          hint: Text("Select..."),
+                          value: time_type_dropdown_chosen,
+                          onChanged: (String? newValue)
+                          {
+                            setState(()
+                            {
+                              time_type_dropdown_chosen = newValue;
+                              print(time_type_dropdown_chosen);
+                            });
+                          },
+                          items: time_type_options.map<DropdownMenuItem<String>>((String dropdown_item)
+                          {
+                            return DropdownMenuItem<String>(
+                              value: dropdown_item,
+                              child: Text(dropdown_item)
+                            );
+                          }
+                          ).toList(),
+                        ),
+                      ),
+                    ]
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text("Duration")
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 20,
+                        ),
+                        child: IntrinsicWidth(
+                          child: TextField(
+                            controller: time_duration_hours_controller,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              isDense: true,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Text("hrs"),
+                      SizedBox(width: 16),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minWidth: 20,
+                        ),
+                        child: IntrinsicWidth(
+                          child: TextField(
+                            controller: time_duration_minutes_controller,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              isDense: true,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Text("mins"),
                     ]
                   ),
                   SizedBox(height: 16),
