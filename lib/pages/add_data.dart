8f2d827ddf3557834191bb50e7afd18fe3e1625a
IcosagonZ <rohanraj3870z@gmail.com>
2026-01-21 20:47:05 +0530

    Added body measurement entry widgets

--- lib/pages/add_data.dart
+++ lib/pages/add_data.dart
@@ -13,7 +13,7 @@ class Page_AddData extends StatefulWidget
 
 class Page_AddData_State extends State<Page_AddData>
 {
-  // Widget variables
+  // Datatyle variables
   final List<String> datatype_dropdown_options = [
     "Academics",
     "Activity",
@@ -23,9 +23,9 @@ class Page_AddData_State extends State<Page_AddData>
     "Vitals",
     "Workout",
   ];
-
   String? datatype_dropdown_chosen;
 
+  // Time and date variables
   DateTime data_datetime = DateTime.now();
 
   TimeOfDay? data_time_chosen;
@@ -49,6 +49,17 @@ class Page_AddData_State extends State<Page_AddData>
   final TextEditingController activity_duration_minutes_controller = TextEditingController();
   final TextEditingController activity_calories_controller = TextEditingController();
   final TextEditingController activity_distance_controller = TextEditingController();
+
+  // Body measurement widgets
+  final TextEditingController bodymeasurement_height_controller = TextEditingController();
+  final TextEditingController bodymeasurement_weight_controller = TextEditingController();
+
+
+  final List<String> bodymeasurement_dropdown_options = [
+    "Height",
+    "Weight",
+  ];
+  String? bodymeasurement_dropdown_chosen;
 
   @override
   void initState()
@@ -441,6 +452,108 @@ class Page_AddData_State extends State<Page_AddData>
                       SizedBox(width: 8),
                       Text("km"),
                     ]
+                  ),
+                  SizedBox(height: 16),
+                  Divider(),
+                ]
+              )
+            ),
+
+            // Body measurements data entry
+            Visibility(
+              visible: datatype_dropdown_chosen=="Body Measurements",
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text("Type")
+                      ),
+                      DropdownButton<String>(
+                        hint: Text("Select data type"),
+                        value: bodymeasurement_dropdown_chosen,
+                        onChanged: (String? newValue)
+                        {
+                          setState(()
+                          {
+                            bodymeasurement_dropdown_chosen = newValue;
+                            print(bodymeasurement_dropdown_chosen);
+                          });
+                        },
+                        items: bodymeasurement_dropdown_options.map<DropdownMenuItem<String>>((String dropdown_item)
+                        {
+                          return DropdownMenuItem<String>(
+                            value: dropdown_item,
+                            child: Text(dropdown_item)
+                          );
+                        }
+                        ).toList(),
+                      )
+                    ]
+                  ),
+                  Visibility(
+                    visible: bodymeasurement_dropdown_chosen=="Height",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Height")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 50,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: bodymeasurement_height_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("cm"),
+                          ]
+                        ),
+                      ]
+                    )
+                  ),
+                  Visibility(
+                    visible: bodymeasurement_dropdown_chosen=="Weight",
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text("Weight")
+                            ),
+                            ConstrainedBox(
+                              constraints: BoxConstraints(
+                                minWidth: 50,
+                              ),
+                              child: IntrinsicWidth(
+                                child: TextField(
+                                  controller: bodymeasurement_weight_controller,
+                                  textAlign: TextAlign.center,
+                                  decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    isDense: true,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text("kg"),
+                          ]
+                        ),
+                      ]
+                    )
                   ),
                   SizedBox(height: 16),
                   Divider(),
